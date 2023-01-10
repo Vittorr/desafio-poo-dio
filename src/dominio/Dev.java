97dfd2ac145737caef66fa4d3d8eef874218be76
Vittorr <paulobjalves@hotmail.com>
2023-01-10 19:59:37 -0300

    desafio pronto

--- src/dominio/Dev.java
+++ src/dominio/Dev.java
@@ -14,29 +14,29 @@ public class Dev {
 
     public void progredir() {
         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        
         if(conteudo.isPresent()) {
             this.conteudosConcluidos.add(conteudo.get());
             this.conteudosInscritos.remove(conteudo.get());
+            
         } else {
+            
             System.err.println("Você não está matriculado em nenhum conteúdo!");
+
         }
     }
 
     public double calcularTotalXp() {
         Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
         double soma = 0;
+        
         while(iterator.hasNext()){
             double next = iterator.next().calcularXp();
             soma += next;
         }
+        
         return soma;
-
-        /*return this.conteudosConcluidos
-                .stream()
-                .mapToDouble(Conteudo::calcularXp)
-                .sum();*/
     }
-
 
     public String getNome() {
         return nome;
@@ -64,14 +64,19 @@ public class Dev {
 
     @Override
     public boolean equals(Object o) {
+        
         if (this == o) return true;
+        
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
+       
         return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
     }
 
     @Override
     public int hashCode() {
+        
         return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+
     }
 }
